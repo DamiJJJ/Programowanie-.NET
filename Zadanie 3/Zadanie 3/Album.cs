f0b4e1b6aa104698dba1bd15a41f773586674ba9
Damian <damian.mrozek@vp.pl>
2022-06-25 18:53:17 +0200

    Zadanie 3

---
+++ Zadanie 3/Zadanie 3/Album.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Zadanie_3
+{
+    public class Album : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        static readonly Dictionary<string, string[]> PowiązaneWłaściwości
+            = new Dictionary<string, string[]>()
+            {
+                ["Tytuł"] = new string[] { "tytułAutorWydawca" },
+                ["Autor"] = new string[] { "tytułAutorWydawca" },
+                ["Wydawca"] = new string[] { "tytułAutorWydawca" },
+                ["Nośnik"] = new string[] { "Nośnik" },
+                ["DataWydania"] = new string[] { "Wiek" },
+            };
+        void OnPropertyChanged(
+            [CallerMemberName] string właściwość = null,
+            HashSet<string> obsłużoneWcześniej = null
+            )
+        {
+            if (obsłużoneWcześniej == null)
+                obsłużoneWcześniej = new HashSet<string>();
+            if (!obsłużoneWcześniej.Contains(właściwość))
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(właściwość));
+                obsłużoneWcześniej.Add(właściwość);
+                if (PowiązaneWłaściwości.ContainsKey(właściwość))
+                    foreach (string powiązanaWłaściwość in PowiązaneWłaściwości[właściwość])
+                        OnPropertyChanged(powiązanaWłaściwość, obsłużoneWcześniej);
+            }
+        }
+
+        private string
+            tytul, autor, wydawca, nosnik;
+        private DateTime?
+            dataWydania;
+
+        public string Tytul
+        {
+            get => tytul;
+            set
+            {
+                tytul = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Autor
+        {
+            get => autor;
+            set
+            {
+                autor = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Wydawca
+        {
+            get => wydawca;
+            set
+            {
+                wydawca = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Nosnik
+        {
+            get => nosnik;
+            set
+            {
+                nosnik = value;
+                OnPropertyChanged();
+            }
+        }
+        public string tytułAutorWydawca => $"{Tytul} - {Autor}, wydawca: {Wydawca}";
+
+        public DateTime? DataWydania
+        {
+            get => dataWydania;
+            set
+            {
+                dataWydania = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ Zadanie 3/Zadanie 3/Model.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Zadanie_3
+{
+    internal class Model
+    {
+        public ObservableCollection<Album> ListaAlbumów { get; } = new ObservableCollection<Album>();
+        public Model()
+        {
+            ListaAlbumów.Add(new Album() { Tytul = "Pretty Boy", Autor = "White 2115", Wydawca = "SBM Label", Nosnik = "CD" });
+            ListaAlbumów.Add(new Album() { Tytul = "Uczta", Autor = "Sanah", Wydawca = "Magic Records" });
+            ListaAlbumów.Add(new Album() { Tytul = "Zeit", Autor = "Rammstein", Wydawca = "Universal Music Group", DataWydania= new DateTime(2022, 4, 29)});
+        }
+
+        internal Album NowyAlbum()
+        {
+            Album nowy = new Album();
+            ListaAlbumów.Add(nowy);
+            return nowy;
+        }
+    }
+}
